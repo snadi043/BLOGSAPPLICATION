d51856001f1f96086a132c9a09f1f2ce1b1c8fe7
snadi043 <snadi043@uottawa.ca>
2026-06-24 11:20:58 -0400

    Updated the initial version of views.py file of the application to handle the basic planned urls for navigating in the application.

--- myblogs/views.py
+++ myblogs/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse, Http404
+
 # Create your views here.
+# This is the response / view which has to be rendered when the landing page of the application is triggered.
+def index(request):
+    return render(request,'myblogs/index.html', 
+        {
+            "pageTitle": "Home"
+        }
+    )
+
+# This is the response / view which has to be rendered when all the posts of the application is triggered.
+def posts(request):
+    return render(request,'myblogs/posts.html', 
+        {
+            "pageTitle": 'All Posts'
+        }
+    )
+
+# This is the response / views which has to be rendered when an individual post from the list of posts of the application is triggered.
+def post_detail(request, postname):
+    return render(request,'myblogs/post.html', 
+        {
+            "pageTitle": '',
+            "postTitle": postname
+        }
+    )
+
+# This is the response / views which has to be rendered when an error in the application is triggered.
+def ErrorPage(request):
+    raise Http404()
