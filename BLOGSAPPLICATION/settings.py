871d97ef1b65e0972949259e042950443249314e
snadi043 <snadi043@uottawa.ca>
2026-06-24 11:02:06 -0400

    Configured the Django Project Settings file (BLOGSAPPLICATION) to handle the internal application's (myblogs) templates and static files using the BASE_DIR global variable.

--- BLOGSAPPLICATION/settings.py
+++ BLOGSAPPLICATION/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'myblogs',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'BLOGSAPPLICATION.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIR = [
+    BASE_DIR / 'static'
+]
