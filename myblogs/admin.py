b2233387cdc364ee2e5a0508ba6666f9ed130b6e
snadi043 <snadi043@uottawa.ca>
2026-07-01 08:33:01 -0400

    Updated the admin.py file to translate the display of the admin panel of the project using the class built in variables like list_display, list_filter and  prepopulated_fields. Also, registered the models in the "admin" file so that Django can handle creation of tables and internally managing the db.sqlite3 file in sync.

--- myblogs/admin.py
+++ myblogs/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Author,Blog,Tag
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "slug", "updatedOn"]
+    list_filter = ["author", "updatedOn", "tags"]
+    prepopulated_fields = {"slug": ("title",)}
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "email"]
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["caption"]
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Tag, TagAdmin)
