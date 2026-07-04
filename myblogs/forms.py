0937d6f468609dddedcc6b9b386738adceac4901
snadi043 <snadi043@uottawa.ca>
2026-07-04 17:45:35 -0400

    Added new rating form field which is an IntegerField and enabled its validations. Also optimized the HTML rendering of the div elements in the review.html file using the for loop and field keyword on the form to access the elements efficiently.

--- myblogs/forms.py
+++ myblogs/forms.py
@@ -9,10 +9,17 @@ class ReviewForm(forms.Form):
         error_messages={"required": "Your name should be atleast 5 to 15 charecters long."})
     
     reviewData = forms.CharField(
-        label="Your Review", 
+        label="Your Review",
+        widget=forms.Textarea(attrs={"rows":"20", "cols":"47"}),
         min_length=25,
-        max_length=100,
+        max_length=250,
         required=True,
-        error_messages={"required": "Your review should be atleast 25 to 100 charecters long."}
-        )
+        error_messages={"required": "Your review should be atleast 25 to 100 charecters long."})
+    
+    rating = forms.IntegerField(
+        label="Your Rating",
+        required=True,
+        min_value= 1,
+        max_value=5,
+        error_messages={"required": "Please enter a rating within 1 to 5."})
 
