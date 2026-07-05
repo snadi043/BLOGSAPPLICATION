97c15748e0f335bf467feba7e9827399297e39b0
snadi043 <snadi043@uottawa.ca>
2026-07-05 11:38:54 -0400

    Refactored the view fucntions to class based views by importing the View from the Django view module and configured the get and post  methods to save the data to the database.

--- myblogs/views.py
+++ myblogs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 
 from django.http import Http404, HttpResponseRedirect
 
+from django.views import View
 from .models import Blog, UserProfileImage
 
 from .forms import ReviewForm, CreateUserProfileForm
@@ -159,12 +160,12 @@ class PostDetailView(DetailView):
 #     )
 
 # This is the response / views which has to be rendered when a user want to write a review on the post from the posts details page of the application.
-def review(request):
-    # On the request object in django, it is possible to access what type of HTTP request are triggered and extract the required data from it.
-    # In the POST method, it is a regular practice to extract the form input data to then store it in the database/local storage depending on the requirement.
-        # entered_username = request.POST['username']
-        # entered_reviewData = request.POST['review-data']
-        # print(entered_username, entered_reviewData)
+# def review(request):
+#     # On the request object in django, it is possible to access what type of HTTP request are triggered and extract the required data from it.
+#     # In the POST method, it is a regular practice to extract the form input data to then store it in the database/local storage depending on the requirement.
+#         # entered_username = request.POST['username']
+#         # entered_reviewData = request.POST['review-data']
+#         # print(entered_username, entered_reviewData)
 
     if request.method == "POST":
         # Instantiating the ReviewForm class to then pass the POST method information to check if the form is valid or not.
@@ -184,13 +185,13 @@ def review(request):
         # methods which is HttpResponseRedirect()
             return HttpResponseRedirect('posts/thankyou')
     
-    # If the method is not a POST method, then this will render the empty ReviewForm.
-    else:
-        form = ReviewForm()
-    return render(request, "myblogs/review.html", 
-            {
-                "form": form
-            })
+#     # If the method is not a POST method, then this will render the empty ReviewForm.
+#     else:
+#         form = ReviewModel(instance = existing_data)
+#     return render(request, "myblogs/review.html", 
+#             {
+#                 "form": form
+#             })
 
 # This is the response / views which has to be rendered to a user after thr review for the post is written.
 # def thankyou(request):
